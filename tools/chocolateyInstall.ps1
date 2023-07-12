0faeb8247ae16adf6146b4fac0ef1180c0aad07d
Vozdyx <vozdyx.dev@gmail.com>
2023-07-12 21:40:27 +0500

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,80 @@
+﻿$instDir    = "${ENV:TEMP}\nvidiainstall" # Folder to move desired components into
+$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName    = 'nvidia-display-driver'
+  destination    = "${ENV:TEMP}\nvidiadriver" # Folder to extract drivers
+  url64          = 'https://us.download.nvidia.com/Windows/536.40/536.40-desktop-win10-win11-64bit-international-dch-whql.exe'
+  checksum64     = '9bdffb76affaeaf38646673898ab697f1aae2122cfe1cb6a07c37c468abc7f19'
+  checksumType64 = 'sha256'
+  silentArgs     = '-s -noreboot'
+  validExitCodes = @(0,1)
+  softwareName   = 'NVIDIA Graphics Driver*'
+}
+
+If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
+  $packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/474.44/474.44-desktop-win8-win7-64bit-international.exe'
+  $packageArgs['checksum64'] = '1e7f9ba6c8dc64d2da7fb28b82731b70a45c37757a339ee0268ec078fa6aaf44'
+}
+
+If ( -not (Get-OSArchitectureWidth -compare 64) ) {
+  Write-Warning "NVIDIA has ended support for 32bit operating systems."
+  Write-Warning "32 bit users should specify version 391.35."
+  Write-Warning "Security patches for 32bit may be available on geforce.com"
+  Write-Error "32 bit no longer supported."
+}
+
+# Remove any previous tempfiles
+Remove-Item "$instDir" -Recurse -Force -ea 0
+New-Item -Path "$instDir" -ItemType Directory
+New-Item -Path "$instDir\GFExperience" -ItemType Directory
+
+# Download driver package as a zip
+$packageArgs['file'] = "${ENV:TEMP}\nvidiadriver.zip"
+Get-ChocolateyWebFile @packageArgs
+
+# Unzip driver package
+Get-ChocolateyUnzip @packageArgs
+
+# Move everything we want to a new folder
+Move-Item ($packageArgs['destination'] + "\Display.Driver"        ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\Display.Optimus"       ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\NVI2"                  ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\NVPCF"                 ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\PhysX"                 ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\EULA.txt"              ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\ListDevices.txt"       ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\setup.cfg"             ) -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\setup.exe"             ) -Destination "$instDir"
+# Must include these legal files to satisfy the installer. GFE will not be installed.
+Move-Item ($packageArgs['destination'] + "\GFExperience\PrivacyPolicy"      ) -Destination "$instDir\GFExperience"
+Move-Item ($packageArgs['destination'] + "\GFExperience\EULA.html"          ) -Destination "$instDir\GFExperience"
+Move-Item ($packageArgs['destination'] + "\GFExperience\FunctionalConsent_*") -Destination "$instDir\GFExperience"
+# I've changed my mind about not including the audio and USB-C driver by default.
+Move-Item ($packageArgs['destination'] + "\HDAudio") -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\PPC"    ) -Destination "$instDir" -ErrorAction SilentlyContinue
+if ( $pp.NV3DVision ) {
+  Move-Item ($packageArgs['destination'] + "\NV3DVision"          ) -Destination "$instDir"
+  Move-Item ($packageArgs['destination'] + "\NV3DVisionUSB.Driver") -Destination "$instDir"
+}
+# Misc new additions
+if ( $pp.Nview ) {
+  Move-Item ($packageArgs['destination'] + "\Display.Nview") -Destination "$instDir"
+}
+if ( $pp.ShadowPlay ) {
+  Move-Item ($packageArgs['destination'] + "\ShadowPlay") -Destination "$instDir"
+}
+if ( $pp.FrameView ) {
+  Move-Item ($packageArgs['destination'] + "\FrameViewSDK") -Destination "$instDir"
+}
+if ( $pp.MSVCRT ) {#MS VC Redist from 2019???
+  Move-Item ($packageArgs['destination'] + "\MSVCRT") -Destination "$instDir"
+}
+
+# Remove unused files
+Remove-Item ($packageArgs['destination']) -Recurse -Force
+
+# Finally, install
+$packageArgs['file'    ] = "$instDir\setup.exe"
+$packageArgs['fileType'] = 'EXE'
+Install-ChocolateyInstallPackage @packageArgs
+
---
+++ update.ps1
@@ -0,0 +1,56 @@
+Import-Module au
+
+$releases64 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&sort1=0&numberOfResults=1'
+$releases7864 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&osID=19&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=0&upCRD=0&sort1=0&numberOfResults=1'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64\s*=\s*)('.*')"                          = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"                     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')"                 = "`$1'$($Latest.ChecksumType64)'"
+      "(?i)(^\s*[$]packageArgs\['url64'\]\s*=\s*)('.*')"      = "`$1'$($Latest.URL7864)'"
+      "(?i)(^\s*[$]packageArgs\['checksum64'\]\s*=\s*)('.*')" = "`$1'$($Latest.Checksum7864)'"
+    }
+    ".\nvidia-display-driver.nuspec" = @{
+      "(?i)(^\s*<releaseNotes>)(.*)" = "`${1}https://us.download.nvidia.com/Windows/$($Latest.Version)/$($Latest.Version)-win11-win10-win8-win7-release-notes.pdf</releaseNotes>"
+    }
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType64 = "sha256"
+  $Latest.Checksum64     = Get-RemoteChecksum -Url $Latest.URL64
+  $Latest.Checksum7864   = Get-RemoteChecksum -Url $Latest.URL7864
+  # export checksums for use with other package
+  sc ..\knownChecksums "$Latest.Checksum64`n$Latest.Checksum7864"
+}
+
+function global:au_GetLatest {
+  # check for presence of exported checksums from other package
+  if (Test-Path ..\knownChecksums) {
+    # overwrite function with known checksums
+    function global:au_BeforeUpdate {
+      $knownChecksums = gc ..\knownChecksums
+      $Latest.ChecksumType64 = "sha256"
+      $Latest.Checksum64     = $knownChecksums[0]
+      $Latest.Checksum7864   = $knownChecksums[1]
+      rm ..\knownChecksums
+    }
+  }
+  $rest64   = Invoke-RestMethod $releases64 -UseBasicParsing
+  $rest7864 = Invoke-RestMethod $releases7864 -UseBasicParsing
+  $version  = $rest64.IDS[0].downloadInfo.Version
+  $url64    = $rest64.IDS[0].downloadInfo.DownloadURL
+  $url7864  = $rest7864.IDS[0].downloadInfo.DownloadURL
+
+  return @{
+    Version = $version
+    URL64   = $url64
+    URL7864 = $url7864
+  }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+  Update-Package -ChecksumFor none
+}
