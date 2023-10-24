4db9a83d489c3c63b70d0e901fd06c2e81e44a52
VozdyxActions <125079012+VozdyxActions@users.noreply.github.com>
2023-10-24 21:52:20 +0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,3 +1,4 @@
+Install-Module au -Force
 Import-Module au
 
 $releases64 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&sort1=0&numberOfResults=1'
